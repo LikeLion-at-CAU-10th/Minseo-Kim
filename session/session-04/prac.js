c772fc5eb3f608d0efc846ca82ebafd3612a14e7
minseo205 <rlaalstj312@naver.com>
2022-04-30 07:55:30 +0900

    portfolio

---
+++ session/session-04/prac.js
@@ -0,0 +1,23 @@
+const minseo = {
+    'name': 'Minseo Kim',
+    'age': 22,
+    'bloodType': 'O'
+}
+
+const age = 22;
+
+if (age > 19){
+    alert("성인입니다!")
+} else if (age <= 19 && age > 10) {
+    alert("청소년입니다!")
+} else {
+    alert("애기네요!")
+}
+
+let age = 1;
+for(let i = 1; i < 6; i++){
+    age += 1;
+    console.log(`${i}년 후의 나이는 ${age}살 입니다.`)
+}
+
+
